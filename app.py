4ff940dc27ec11ac175b82a0efa96bfc6e66b529
NusratJahanMahina <nusratjahanmahina792@gmail.com>
2026-08-17 14:29:10 +0600

    correct

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 from flask import Flask
 from login_register import register_login_routes
-from agent_routes import register_agent_routes   # 👈 ADD THIS
+from agent_routes import register_agent_routes   
 
 app = Flask(__name__)
 app.secret_key = 'krishibondhu_secret_key_2025'
 app.permanent_session_lifetime = 3600
 
 # Register all routes
-register_login_routes(app)
-register_agent_routes(app)   # 👈 ADD THIS
-
+register_login_routes(app)   
+register_agent_routes(app)  
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
